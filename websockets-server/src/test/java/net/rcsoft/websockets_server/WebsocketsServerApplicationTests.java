0c76daa55d8db8236cca02be2719751b6296caf5
recampelos <recampelos@gmail.com>
2025-02-06 23:49:33 +0000

    Projects scaffolding
    Added initial projects configurations

---
+++ websockets-server/src/test/java/net/rcsoft/websockets_server/WebsocketsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rcsoft.websockets_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
